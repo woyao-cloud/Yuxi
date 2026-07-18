64073a35608ec96957695793afdb193a2b0b00c8
dcdu <dcdu@cn.ibm.com>
2026-07-18 11:26:09 +0800

    fix_bugs

--- web-react/vite.config.ts
+++ web-react/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-
+// line 20 docker-compose.yml 修改为  target: env.VITE_API_URL || 'http://api:5050',
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig(({ mode }) => {
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '^/api': {
-          target: env.VITE_API_URL || 'http://api:5050',
+          target: env.VITE_API_URL || 'http://localhost:5050',
           changeOrigin: true
         }
       },
